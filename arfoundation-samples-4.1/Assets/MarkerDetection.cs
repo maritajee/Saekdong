54737bff7e077552d003e74f640447d2d86c40eb
maritajee <maritajee@gmail.com>
2022-08-30 17:12:06 +0900

    Image detection succeeded

--- arfoundation-samples-4.1/Assets/MarkerDetection.cs
+++ arfoundation-samples-4.1/Assets/MarkerDetection.cs
@@ -10,18 +10,36 @@ public class MarkerDetection : MonoBehaviour
     public Canvas canvas01;
     public Canvas canvas02;
 
+    public string targetImgName;
+    public GameObject target3dObj;
+    ARTrackedImageManager arTrackImageManager;
+    Dictionary<string, GameObject> _prefabDic = new Dictionary<string, GameObject>();
+
+    private bool imagedetected = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        arTrackImageManager = GetComponent<ARTrackedImageManager>();
+        _prefabDic.Add(targetImgName, target3dObj);
+        arTrackImageManager.trackedImagesChanged += ImageStateChanged;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(ARTrackedImage.referenceImage)
+        if(imagedetected)
         {
+            canvas01.gameObject.SetActive(false);
+            canvas02.gameObject.SetActive(true);
+        }
+    }
 
+    void ImageStateChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
+        {
+            Debug.Log("image tracking add!!" + trackedImage.name);
+            imagedetected = true;
         }
     }
 }
