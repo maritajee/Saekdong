7b00035ee5f529f0e0d6a4ea3adab1844b31fbbc
maritajee <maritajee@gmail.com>
2022-10-02 13:39:36 +0900

    fix text effect

--- arfoundation-samples-4.1/Assets/CanvasEffect.cs
+++ arfoundation-samples-4.1/Assets/CanvasEffect.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class CanvasEffect : MonoBehaviour
 {
+    public GameObject next_txt1;
+    public GameObject next_txt2;
     public GameObject next_btn;
-    public GameObject next_txt;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,9 @@ public class CanvasEffect : MonoBehaviour
     IEnumerator WaitSec(float sec)
     {
         yield return new WaitForSeconds(sec);
+        next_txt1.SetActive(true);
+        yield return new WaitForSeconds(sec);
+        next_txt2.SetActive(true);
         next_btn.SetActive(true);
-        next_txt.SetActive(true);
     }
 }
